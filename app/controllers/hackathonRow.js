958d77e6994b121522f4c8c333c4d8b5f459be27
Bilal Quadri <bilalquadri92@gmail.com>
2013-04-07 09:51:48 -0400

    More event firing

--- app/controllers/hackathonRow.js
+++ app/controllers/hackathonRow.js
@@ -3,7 +3,11 @@ var args = arguments[0] || {};
 $.name.text = args.name;
 $.dateRange.text = args.display_date;
 
-$.hackathon.addEventListener('click', function(e) {
+function openDetailWindow(e) {
   var detailView = Alloy.createController('hackathonDetail', args).getView();
   detailView.open();
-});
+}
+$.hackathon.addEventListener('click', openDetailWindow);
+$.name.addEventListener('click', openDetailWindow);
+$.location.addEventListener('click', openDetailWindow);
+$.dateRange.addEventListener('click', openDetailWindow);
