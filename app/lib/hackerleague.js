e545b88d10e9496ae349bfc00ce0c7c08f60d854
Bilal Quadri <bilalquadri92@gmail.com>
2013-04-07 09:37:53 -0400

    Nearby hackathon queries

--- app/lib/hackerleague.js
+++ app/lib/hackerleague.js
@@ -19,3 +19,18 @@ exports.getHacks = (function(hackathonID, callback) {
     },
   });
 });
+
+exports.getNearbyHackathons = (function(state, callback) {
+  request({
+    method: 'GET',
+    url: 'hackathons.json?limit=900',
+    callback: function(err, response) {
+      var hackathons = JSON.parse(response.responseData);
+      var nearby = _.filter(hackathons, function(hackathon) {
+        return (hackathon && hackathon.state && hackathon.location && hackathon.location.state) &&
+          hackathon.state !== "complete" && (hackathon.location.state === state || hackathon.location.state === "Anywhere")
+      });
+      callback(null, nearby);
+    },
+  });
+});
