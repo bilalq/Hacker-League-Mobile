854fa25160a220cdbe491fa4c27ddd47765737a3
Gerard O'Neill <grardb@gmail.com>
2013-04-07 07:58:47 -0400

    momentjs

--- app/controllers/hackathons.js
+++ app/controllers/hackathons.js
@@ -6,6 +6,14 @@ hackerleague.getHackathons(function(err, response) {
   for (var i = 0; i < response.length; i++) {
     var hackathon = response[i];
     var row = Alloy.createController('hackathonRow', hackathon).getView();
+
+    var startDate = new Date(hackathon.start_date);
+    var endDate = new Date(hackathon.end_date);
+    var dateFormat = 'MMMM Do YYYY';
+    var startDateStr = moment(startDate).format(dateFormat);
+    var endDateStr = moment(startDate).format(dateFormat);
+
+    hackathon.display_date = startDateStr + ' - ' + endDateStr;
     $.hackathons.add(row);
   }
 
