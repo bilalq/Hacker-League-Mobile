113d5ff24c0126f60aa67fe88d46ab51c7661649
Bilal Quadri <bilalquadri92@gmail.com>
2013-04-07 09:42:09 -0400

    Added event for opening detail window

--- app/controllers/hackathonRow.js
+++ app/controllers/hackathonRow.js
@@ -2,3 +2,8 @@ var args = arguments[0] || {};
 
 $.name.text = args.name;
 $.dateRange.text = args.display_date;
+
+$.hackathon.addEventListener('click', function(e) {
+  var detailView = Alloy.createController('hackathonDetail', args).getView();
+  detailView.open();
+});
