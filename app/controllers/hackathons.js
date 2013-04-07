84278296e0acdcff8b1986ad41ada2ab19bcca55
Gerard O'Neill <grardb@gmail.com>
2013-04-07 09:36:12 -0400

    showing date range in list

--- app/controllers/hackathons.js
+++ app/controllers/hackathons.js
@@ -5,15 +5,16 @@ hackerleague.getHackathons(function(err, response) {
 
   for (var i = 0; i < response.length; i++) {
     var hackathon = response[i];
-    var row = Alloy.createController('hackathonRow', hackathon).getView();
 
-    var startDate = new Date(hackathon.start_date);
-    var endDate = new Date(hackathon.end_date);
+    var startDate = new Date(hackathon.start_time);
+    var endDate = new Date(hackathon.end_time);
     var dateFormat = 'MMMM Do YYYY';
     var startDateStr = moment(startDate).format(dateFormat);
-    var endDateStr = moment(startDate).format(dateFormat);
+    var endDateStr = moment(endDate).format(dateFormat);
 
     hackathon.display_date = startDateStr + ' - ' + endDateStr;
+
+    var row = Alloy.createController('hackathonRow', hackathon).getView();
     $.hackathons.add(row);
   }
 
