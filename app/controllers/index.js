44aa533ec46119dd6db45a355213917af891aef7
Bilal Quadri <bilalquadri92@gmail.com>
2013-04-07 03:14:24 -0400

    Hackathons view

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,3 +5,8 @@ var hl = require('hackerleague');
 hl.getHackathons(function(response) {
   console.log(response[0]);
 });
+
+setTimeout(function() {
+  var hackthons = Alloy.createController('hackathons').getView();
+  hackathons.open():
+}, 1000);
